fedbea4b09e3afe97d299154a4949cfe346caea2
Joren Terpstra <jorenterpstra@gmail.com>
2024-11-25 20:47:17 +0100

    fix?

--- vim/engine.py
+++ vim/engine.py
@@ -43,6 +43,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
 
         if mixup_fn is not None:
             samples, targets = mixup_fn(samples, targets)
+            torch.cuda.synchronize()
         print(2)
         if args.cosub:
             samples = torch.cat((samples,samples),dim=0)
