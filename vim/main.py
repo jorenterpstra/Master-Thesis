1d4646a7e3dae9e8ad23fcf9a8dad5dc8d428cdd
Joren Terpstra <jorenterpstra@gmail.com>
2024-11-25 19:25:29 +0100

    moved print statement

--- vim/main.py
+++ vim/main.py
@@ -220,10 +220,10 @@ def get_args_parser():
 
 
 def main(args):
+    print(args)
     utils.init_distributed_mode(args)
 
-    print(args)
-
+    
     if args.distillation_type != 'none' and args.finetune and not args.eval:
         raise NotImplementedError("Finetuning with distillation not yet supported")
 
