eb22cb270a2d2172eaa488cc387db0f42f16a10b
Joren Terpstra <jorenterpstra@gmail.com>
2024-11-25 19:31:08 +0100

    debugging

--- vim/utils.py
+++ vim/utils.py
@@ -227,7 +227,7 @@ def init_distributed_mode(args):
         return
 
     args.distributed = True
-
+    print(args.rank, "\n", args.gpu)
     torch.cuda.set_device(args.gpu)
     args.dist_backend = 'nccl'
     print('| distributed init (rank {}): {}'.format(
