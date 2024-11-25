4cb5e01736b44e73e84eb8ac6cf9b6df87967b91
Joren Terpstra <jorenterpstra@gmail.com>
2024-11-25 20:45:12 +0100

    debuzz

--- vim/engine.py
+++ vim/engine.py
@@ -37,22 +37,19 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
         # count += 1
         # if count > 20:
         #     break
-        print("samples.shape: ", samples.shape)
-        print("targets.shape: ", targets.shape)
-        print(f"Data to device {device}")
+        print(1)
         samples = samples.to(device, non_blocking=True)
         targets = targets.to(device, non_blocking=True)
-        print(f"Data to device {device} done")
 
         if mixup_fn is not None:
             samples, targets = mixup_fn(samples, targets)
-            
+        print(2)
         if args.cosub:
             samples = torch.cat((samples,samples),dim=0)
-            
+        print(3)
         if args.bce_loss:
             targets = targets.gt(0.0).type(targets.dtype)
-         
+        print(4) 
         with amp_autocast():
             outputs = model(samples, if_random_cls_token_position=args.if_random_cls_token_position, if_random_token_rank=args.if_random_token_rank)
             # outputs = model(samples)
@@ -64,13 +61,13 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
                 loss = loss + 0.25 * criterion(outputs[1], targets) 
                 loss = loss + 0.25 * criterion(outputs[0], outputs[1].detach().sigmoid())
                 loss = loss + 0.25 * criterion(outputs[1], outputs[0].detach().sigmoid()) 
-
+        print(5)
         if args.if_nan2num:
             with amp_autocast():
                 loss = torch.nan_to_num(loss)
-
+        print(6)
         loss_value = loss.item()
-
+        print(7)
         if not math.isfinite(loss_value):
             print("Loss is {}, stopping training".format(loss_value))
             if args.if_continue_inf:
@@ -78,9 +75,9 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
                 continue
             else:
                 sys.exit(1)
-
+        print(8)
         optimizer.zero_grad()
-
+        print(9)
         # this attribute is added by timm on one optimizer (adahessian)
         if isinstance(loss_scaler, timm.utils.NativeScaler):
             is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
@@ -91,11 +88,12 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
             if max_norm != None:
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
             optimizer.step()
-
+        print(10)
         torch.cuda.synchronize()
+        print(11)
         if model_ema is not None:
             model_ema.update(model)
-
+        print(12)
         metric_logger.update(loss=loss_value)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
     # gather the stats from all processes
