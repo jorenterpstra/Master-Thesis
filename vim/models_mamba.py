e1690eb9c0a726009ba8817ba79aa4afaae48609
Joren Terpstra <jorenterpstra@gmail.com>
2024-11-26 15:05:36 +0100

    sdf

--- vim/models_mamba.py
+++ vim/models_mamba.py
@@ -543,6 +543,7 @@ class VisionMamba(nn.Module):
             raise NotImplementedError
 
     def forward(self, x, return_features=False, inference_params=None, if_random_cls_token_position=False, if_random_token_rank=False):
+        print("Forward pass")
         x = self.forward_features(x, inference_params, if_random_cls_token_position=if_random_cls_token_position, if_random_token_rank=if_random_token_rank)
         if return_features:
             return x
