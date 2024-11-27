2da7cc85be63b9a94e05b819a5079e1b564be6b0
Joren Terpstra <jorenterpstra@gmail.com>
2024-11-27 15:07:12 +0100

    meer subset

--- vim/scripts/pt-vim-t-subset.sh
+++ vim/scripts/pt-vim-t-subset.sh
@@ -6,10 +6,10 @@ CUDA_VISIBLE_DEVICES=1 torchrun \
     --nproc_per_node=1 \
     main.py \
     --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
-    --batch-size 128 \
+    --batch-size 64 \
     --drop-path 0.0 \
     --weight-decay 0.1 \
-    --num_workers 25 \
+    --num_workers 16 \
     --data-path /storage/scratch/6403840/data/imagenet_subset \
     --output_dir ./output/vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
     --no_amp \
