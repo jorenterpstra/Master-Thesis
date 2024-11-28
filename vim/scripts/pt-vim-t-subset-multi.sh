7a19c4aad56324ab18f3056b4ff91345f808248e
Joren Terpstra <jorenterpstra@gmail.com>
2024-11-28 14:02:28 +0100

    foutje

--- vim/scripts/pt-vim-t-subset-multi.sh
+++ vim/scripts/pt-vim-t-subset-multi.sh
@@ -5,7 +5,7 @@
 CUDA_VISIBLE_DEVICES=2,3 torchrun \
     --nproc_per_node=2 \
     --rdzv_backend=c10d   \
-    --rdzv-endpoint=localhost:0
+    --rdzv-endpoint=localhost:0 \
     main.py \
     --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
     --batch-size 64 \
