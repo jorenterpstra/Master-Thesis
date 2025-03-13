66900c0164a90cf9abd85cfd210ea785b06b2752
Joren Terpstra <jorenterpstra@gmail.com>
2025-03-13 17:44:35 +0100

    debug

--- vim/scripts/pt-vim-t-subset.sh
+++ vim/scripts/pt-vim-t-subset.sh
@@ -30,7 +30,8 @@ torchrun \
     --no_amp \
     --pin-mem \
     --mixup 0.0 \
-    --cutmix 0.0 
+    --cutmix 0.0 \
+    --debug 
 # CUDA_VISIBLE_DEVICES=0 python main.py \
 #     --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
 #     --batch-size 64 \
