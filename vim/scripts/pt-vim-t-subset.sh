db79d6fcf5d2a28f5a17e789ca4ce96bdf4845c3
Joren Terpstra <jorenterpstra@gmail.com>
2025-03-13 17:50:06 +0100

    zo dan?

--- vim/scripts/pt-vim-t-subset.sh
+++ vim/scripts/pt-vim-t-subset.sh
@@ -17,9 +17,7 @@ module load cuda/11.8
 source ~/.bashrc
 conda activate vim
 
-torchrun \
-    --nproc_per_node=1 \
-    main.py \
+python main.py \
     --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
     --batch-size 64 \
     --drop-path 0.0 \
