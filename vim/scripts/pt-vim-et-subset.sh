266cdc57d1286e73d0450ee3f148a1c5aa7633ef
Joren Terpstra <jorenterpstra@gmail.com>
2025-04-28 22:47:56 +0200

    sd

--- vim/scripts/pt-vim-et-subset.sh
+++ vim/scripts/pt-vim-et-subset.sh
@@ -29,9 +29,7 @@ export MKL_NUM_THREADS=2
 
 # Select the GPUs with the least memory usage
 
-torchrun --ntasks=2 \
-    --ntasks-per-node=2 \
-    --nproc_per_node=1 \
+torchrun --nproc_per_node=1 \
     --nnodes=1 \
     --master_addr=$MASTER_ADDR \
     --master_port=$MASTER_PORT \
