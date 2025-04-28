60faa2df1e24d8d45c47d05bd6fd18bfc95b5ecd
Joren Terpstra <jorenterpstra@gmail.com>
2025-04-28 14:55:10 +0200

    laatste ding dan

--- vim/utils.py
+++ vim/utils.py
@@ -292,7 +292,7 @@ def init_distributed_mode(args):
         print(f"Error during barrier: {e}")
 
     # Uncomment if needed - disable printing for non-master processes
-    setup_for_distributed(args.rank == 0) 
+    setup_for_distributed(args.gpu == 0) 
 
     if args.debug:
         print(f'| Process {args.rank} done with init process group on GPU {args.gpu}', flush=True)
