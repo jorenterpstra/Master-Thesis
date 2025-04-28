97476f59dc3923dff0f41bbbdb8c38762e193657
Joren Terpstra <jorenterpstra@gmail.com>
2025-04-28 23:45:07 +0200

    geen barrier

--- vim/utils.py
+++ vim/utils.py
@@ -321,7 +321,7 @@ def init_distributed_mode(args):
         print(f"| Process {args.rank} passed all_reduce test", flush=True)
         
         # Replace standard barrier with timeout version
-        torch.distributed.barrier(timeout=datetime.timedelta(seconds=60))
+        #torch.distributed.barrier()
         print(f'| Process {args.rank} passed barrier on GPU {args.gpu}', flush=True)
     except Exception as e:
         print(f"Error during barrier: {e}")
