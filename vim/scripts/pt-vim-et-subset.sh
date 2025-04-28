379faf5d8f2a69e157a616cf2f84a50e369755c7
Joren Terpstra <jorenterpstra@gmail.com>
2025-04-28 23:29:14 +0200

    dit proberen

--- vim/scripts/pt-vim-et-subset.sh
+++ vim/scripts/pt-vim-et-subset.sh
@@ -22,9 +22,9 @@ conda activate mamba
 
 export OMP_NUM_THREADS=2
 export MKL_NUM_THREADS=2
-export NCCL_DEBUG=INFO
-export NCCL_ASYNC_ERROR_HANDLING=1
-export PYTHONUNBUFFERED=1
+# export NCCL_DEBUG=INFO
+# export NCCL_ASYNC_ERROR_HANDLING=1
+# export PYTHONUNBUFFERED=1
 
 # Select the GPUs with the least memory usage
 
@@ -41,9 +41,7 @@ srun --ntasks=2 --nodes=1 --gpus-per-task=1 --export=ALL\
     --no_amp \
     --pin-mem \
     --mixup 0.0 \
-    --cutmix 0.0 \
-    --distributed \
-    --debug
+    --cutmix 0.0 
 
 # CUDA_VISIBLE_DEVICES=0 python main.py \
 #     --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
