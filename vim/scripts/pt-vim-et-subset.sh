dfb3c070912ffbb20c65dbf3bd4b2ff00d9cc7d7
Joren Terpstra <jorenterpstra@gmail.com>
2025-04-29 13:57:20 +0200

    geen debug meer, het werkt!!!!

--- vim/scripts/pt-vim-et-subset.sh
+++ vim/scripts/pt-vim-et-subset.sh
@@ -50,8 +50,7 @@ python -m torch.distributed.run \
     --no_amp \
     --pin-mem \
     --mixup 0.0 \
-    --cutmix 0.0 \
-    --debug
+    --cutmix 0.0 
 
 # CUDA_VISIBLE_DEVICES=0 python main.py \
 #     --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 \
