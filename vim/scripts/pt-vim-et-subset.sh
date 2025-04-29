74894448c584d98a492c74c177e1b49e635aef3c
Joren Terpstra <jorenterpstra@gmail.com>
2025-04-29 13:23:31 +0200

    nu dan?

--- vim/scripts/pt-vim-et-subset.sh
+++ vim/scripts/pt-vim-et-subset.sh
@@ -38,6 +38,9 @@ export RDZV_PORT=29400
 # Select the GPUs with the least memory usage
 
 torchrun \
+    --nproc_per_node=2 \
+    --nnodes=1 \
+    --node_rank=0 \
     --rdzv_id=$SLURM_JOB_ID \
     --rdzv_backend=c10d \
     --rdzv_endpoint="$RDZV_HOST:$RDZV_PORT" \
