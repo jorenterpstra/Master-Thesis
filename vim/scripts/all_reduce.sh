9b203143806548b4dd7ab01237452bdccf06cf5d
Joren Terpstra <jorenterpstra@gmail.com>
2025-04-29 13:46:46 +0200

    sdf

--- vim/scripts/all_reduce.sh
+++ vim/scripts/all_reduce.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=vim-extra-tiny
+#SBATCH --time=10-00:00:00
+#SBATCH --ntasks-per-node=2
+#SBATCH --gpus-per-node=2
+#SBATCH --cpus-per-task=1
+#SBATCH --gpu-freq=medium # Request medium priority GPU access
+
 export MASTER_ADDR=$(hostname)
 export MASTER_PORT=29500
 python -m torch.distributed.run \
