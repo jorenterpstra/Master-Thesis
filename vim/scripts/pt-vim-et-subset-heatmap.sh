f50fa37a8e2ed90ad83a9cea65f65fe703027f79
Joren Terpstra <jorenterpstra@gmail.com>
2025-06-04 11:50:19 +0200

    random token rank

--- vim/scripts/pt-vim-et-subset-heatmap.sh
+++ vim/scripts/pt-vim-et-subset-heatmap.sh
@@ -40,7 +40,8 @@ python -m torch.distributed.run \
     --data-path /storage/scratch/6403840/data/imagenet-tiny \
     --heatmap-path /storage/scratch/6403840/data/imagenet-tiny \
     --return-rankings \
-    --output_dir ./output/vim_extra_tiny_custom_transforms_heatmap_pos_embed \
+    --output_dir ./output/vim_extra_tiny_custom_transforms_random \
+    --if_random_token_rank \
     --no_amp \
     --pin-mem \
     --mixup 0.0 \
