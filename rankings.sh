2febfab8cc065ed92c08c7eb058ed6cc24cf1473
Joren Terpstra <jorenterpstra@gmail.com>
2025-06-25 02:00:54 +0200

    minder cpu

--- rankings.sh
+++ rankings.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=get-rankings
 #SBATCH --time=2-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 
 python patch_rankings.py \
     --data-path /storage/scratch/6403840/data/imagenet-tiny/val \
